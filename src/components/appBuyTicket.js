c77b06b23e76a9a02dc93e574a8ad2e5e859ab14
MelynaPernia <mebipeos@outlook.com>
2018-03-20 06:29:23 -0500

    Creando el componente Galeria y GaleriaType, además agregando los datos

--- src/components/appBuyTicket.js
+++ src/components/appBuyTicket.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Cazuela from './Cazuela'
+import Galeria from './Galeria'
+const data = [{
+  name: 'GALERIA VISIBIL',
+  description: 'Visib. Restringida',
+  nivel: '3ER'
+},
+{
+  name: 'GALERIA BUENA',
+  description: 'VISIBILIDAD',
+  nivel: '3ER'
+},
+{
+  name: 'GALERIA VISIBIL',
+  description: 'Visib. Restringida',
+  nivel: '3ER'
+}]
 const AppBuyTicket = () => (
   <div>
-    <Cazuela />
+    <Galeria data = {data} />
   </div>
 )
 
